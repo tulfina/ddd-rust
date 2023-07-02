be9eadebcdc4c1866b9bef760cee0cf59e4399d4
Tulfina <alisha@tolfx.dev>
2023-07-02 17:52:23 +0200

    feat: working on aggregate (#1)

--- src/aggregate.rs
+++ src/aggregate.rs
@@ -1,1 +1,10 @@
-// Implement
+use crate::command::Command;
+use crate::event::Event;
+
+pub trait Aggregate<State> {
+    type Commands;
+    type Events;
+    fn sink(&mut self, command: &Command<State>) -> Result<(), String>;
+    fn apply(&mut self, event: &Event<State>) -> Result<(), String>;
+    fn state(&self) -> State;
+}
